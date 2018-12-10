94bad8cd8234a1afd55512904cf856508a70e82d
Blouk <nicolas@blouk.com>
2018-12-10 14:51:15 +0100

    [ADD] add scss helpers, variables & main, create the module

---
+++ theme_create/__manifest__.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+{
+    'name':  'Create a theme',
+    'category': 'Theme/Creative',
+    'summary': 'Create a theme',
+    'version': '0.2',
+    'description': """
+Description.
+    """,
+    'depends': ['website_sale'],
+
+    'data': [
+        # assets views
+        'views/assets.xml',
+    ],
+
+    'installable': True,
+    'application': True,
+}
